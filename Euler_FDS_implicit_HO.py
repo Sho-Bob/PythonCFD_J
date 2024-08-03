Fixed Jacobian matrix

Euler_FDS_implicit_HO.py
@@ -4,7 +4,7 @@ import matplotlib.animation as animation
 import sys
 
 jmax = 101
-dt = 0.002
+dt = 0.01
 
 gamma = 1.4
 
@@ -16,14 +16,15 @@ PE = 0.1
 RHOE = 0.1
 UE = 0.0
 
-xmin, xmid, xmax = 0.0, 0.5, 1.0
+xmin, xmid, xmax = 0.0, 0.5, 1.0 # Sod problem
+# xmin, xmid, xmax = -4.0, 0.0, 4.0 # Shu Osher problem
 x = np.linspace(xmin, xmax, jmax)
 
 dx = (xmax - xmin) / (jmax - 1)
 
 dtdx = dt / dx
 
-def init():
+def init_Sod():
     Q = np.zeros([jmax, 3])
 
     Q[x <= xmid, 0] = RHOI
@@ -36,6 +37,22 @@ def init():
 
     return Q
 
+def init_Shu_Osher():
+    Q = np.zeros([jmax,3])
+    rhoL = 3.857143
+    uL = 2.629369
+    pL = 10.333
+    pR=1.0
+    Q[x<-1.0,0] = rhoL 
+    Q[x<-1.0,1] = rhoL*uL
+    Q[x<-1.0,2] = (pL/(gamma-1.0)+ 0.5*rhoL*uL**2)
+    # Q[x>=-4.0,0] = 1.0+0.2*np.sin(5.0*x) 
+    Q[x >= -1, 0] = 1.0 + 0.2 * np.sin(5.0 * x[x >= -1])
+    Q[x>=-1,1] = 0.0
+    Q[x>=-1.0,2] = pR/(gamma-1.0)
+
+    return Q
+
 def calc_CFL(Q):
     rho, rhou, e = Q[:, 0], Q[:, 1], Q[:, 2]
     
@@ -45,6 +62,59 @@ def calc_CFL(Q):
     c = np.sqrt(gamma * p / rho)
     sp = c + np.abs(u)
     return max(sp) * dtdx   
+
+def HLLC(QL, QR, E):
+    for j in range(jmax - 1):  
+        rhoL, uL, pL = QL[j, 0], QL[j, 1], QL[j, 2]
+        rhoR, uR, pR = QR[j + 1, 0], QR[j + 1, 1], QR[j + 1, 2]
+
+        rhouL = rhoL * uL
+        rhouR = rhoR * uR
+
+        EL = pL / (gamma - 1.0) + 0.5 * rhoL * uL ** 2
+        ER = pR / (gamma - 1.0) + 0.5 * rhoR * uR ** 2
+
+        HL = (EL + pL) / rhoL
+        HR = (ER + pR) / rhoR
+        
+        cL = np.sqrt((gamma - 1.0) * (HL - 0.5 * uL ** 2))
+        cR = np.sqrt((gamma - 1.0) * (HR - 0.5 * uR ** 2))
+
+        f_L = np.array([rhouL, rhouL * uL + pL, (EL + pL) * uL])
+        f_R = np.array([rhouR, rhouR * uR + pR, (ER + pR) * uR])
+
+        sL = uL - cL
+        sR = uR + cR
+
+        # Calculate star region values
+        c_L = rhoL * (sL - uL)
+        c_R = rhoR * (sR - uR)
+
+        u_star = (-c_L * uL + c_R * uR - (pR - pL)) / (-c_L + c_R)
+        p_star = pL + c_L * (u_star - uL)
+        rhoL_star = rhoL * (sL - uL) / (sL - u_star)
+        rhoR_star = rhoR * (sR - uR) / (sR - u_star)
+        EL_star = ((sL - uL) * EL - pL * uL + p_star * u_star) / (sL - u_star)
+        ER_star = ((sR - uR) * ER - pR * uR + p_star * u_star) / (sR - u_star)
+
+        if sL > 0:
+            E[j] = f_L
+        elif sL <= 0 and u_star >= 0:
+            E[j] = np.array([
+                rhouL - sL * (rhoL_star * u_star - rhouL),
+                (rhouL * uL + pL - sL * (rhoL_star * u_star**2 + p_star - (rhouL * uL + pL))) / (1.0 - sL),
+                (EL + pL) * uL - sL * ((EL_star + p_star) * u_star - (EL + pL) * uL) / (1.0 - sL)
+            ])
+        elif u_star < 0 and sR >= 0:
+            E[j] = np.array([
+                rhouR - sR * (rhoR_star * u_star - rhouR),
+                (rhouR * uR + pR - sR * (rhoR_star * u_star**2 + p_star - (rhouR * uR + pR))) / (1.0 - sR),
+                (ER + pR) * uR - sR * ((ER_star + p_star) * u_star - (ER + pR) * uR) / (1.0 - sR)
+            ])
+        elif sR < 0:
+            E[j] = f_R
+
+    return E
 
 def Roe_flux(QL, QR, E):
     for j in range(jmax - 1):  
@@ -153,9 +223,9 @@ def compute_jacobian(Q):
         R = np.array([[1.0,1.0,1.0],
                      [velo_x-acoustic,        velo_x,        velo_x+acoustic],
                      [total_h-velo_x*acoustic, 0.5*velo_x**2, total_h+velo_x*acoustic]])
-        R_inv = np.array([[0.5*(b1+velo_x/acoustic), -0.5*(acoustic+b2*velo_x), 0.5*b2],
+        R_inv = np.array([[0.5*(b1+velo_x/acoustic), -0.5*(1/acoustic+b2*velo_x), 0.5*b2],
                          [1.0-b1,                     b2*velo_x,                  -b2],
-                         [0.5*(b1-velo_x/acoustic),  -0.5*(b2*velo_x-acoustic), 0.5*b2]])
+                         [0.5*(b1-velo_x/acoustic),  -0.5*(b2*velo_x-1/acoustic), 0.5*b2]])
         lambda_p = np.array([[0.5*(velo_x-acoustic+abs(velo_x-acoustic)), 0.0,                     0.0],
                              [0.0,                                        0.5*(velo_x+abs(velo_x)),0.0],
                              [0.0,                                        0.0                     ,0.5*(velo_x+acoustic+abs(velo_x+acoustic))]])
@@ -187,7 +257,8 @@ def Roe_FDS(Q, order, kappa, nmax, print_interval, time_integration, T_order):
             coefs = [0.5, 1.0]
             for coef in coefs:
                 QL, QR = MUSCL(Qold, order, kappa)
-                Roe_flux(QL, QR, E)
+                # Roe_flux(QL, QR, E)
+                HLLC(QL, QR, E)
                 
                 for j in range(1, jmax - 1):
                     Qold[j] = Q[j] - coef * dtdx * (E[j] - E[j-1])
@@ -216,6 +287,7 @@ def Roe_FDS(Q, order, kappa, nmax, print_interval, time_integration, T_order):
                 for m in range(internal_itr):
                     QL, QR = MUSCL(Q_m, order, kappa)
                     Roe_flux(QL, QR, E)
+                    # HLLC(QL, QR, E)
                     
                     A, A_p, A_n, sigma = compute_jacobian(Q_m_cons)
                     
@@ -235,13 +307,15 @@ def Roe_FDS(Q, order, kappa, nmax, print_interval, time_integration, T_order):
                     Q_m[:] = Q_m_cons[:]
                     
                     dq_max = np.max(dq)
+                
                     if (dq_max < 1.e-4):
                         print(f'Iteration {m}, dq_max: {dq_max}')
                         break
             elif(T_order == 2):
                 for m in range(internal_itr):
                     QL, QR = MUSCL(Q_m, order, kappa)
-                    Roe_flux(QL, QR, E)
+                    # Roe_flux(QL, QR, E)
+                    HLLC(QL, QR, E)
                     
                     A, A_p, A_n, sigma = compute_jacobian(Q_m_cons)
                     
@@ -273,12 +347,8 @@ def Roe_FDS(Q, order, kappa, nmax, print_interval, time_integration, T_order):
 
             Qold2[:] = Qold[:]
             Qold[:] = Q_m_cons[:]
-            # Qold[0] = Q[0]
-            # Qold[-1] = Q[-1]
-            # Qold2[0] = Q[0]
-            # Qold2[-1] = Q[-1]
             Q[:] = Q_m_cons[:]
-    
+            print("step done")
     return results
 
 def update_plot(frame, x, line):
@@ -293,13 +363,16 @@ if __name__ == "__main__":
 
     kappa = 0
     time_integration = 1
-    time_order = 2
+    time_order = 1
 
-    Q1 = init()
-    Q = init()
-    Q2 = init()
-    results_exp = Roe_FDS(Q1, order, kappa, nmax, print_interval, 0, time_order)
-    results = Roe_FDS(Q, order, kappa, nmax, print_interval, time_integration, time_order)
+    # Q1 = init_Sod()
+    # Q = init_Sod()
+    Q2 = init_Sod()
+    # Q1 = init_Shu_Osher()
+    # Q = init_Shu_Osher()
+    # Q2 = init_Shu_Osher()
+    # results_exp = Roe_FDS(Q1, order, kappa, nmax, print_interval, 0, time_order)
+    # results = Roe_FDS(Q, order, kappa, nmax, print_interval, time_integration, time_order)
     results2 = Roe_FDS(Q2, order, kappa, nmax, print_interval, time_integration, 1)
 
     fig, ax = plt.subplots(figsize=(7, 7), dpi=100)
@@ -307,8 +380,8 @@ if __name__ == "__main__":
     ax.set_xlabel('x')
     ax.set_ylabel(r'$\rho$')
     ax.grid(color='black', linestyle='dotted', linewidth=0.5)
-    line, = ax.plot(x, results_exp[99][:, 0], color='black', linewidth=1.5,label='RK2nd')
-    line, = ax.plot(x, results[99][:, 0], color='red', linewidth=1.5,label='LU-SGS 2nd')
+    # line, = ax.plot(x, results_exp[99][:, 0], color='black', linewidth=1.5,label='RK2nd')
+    # line, = ax.plot(x, results[99][:, 0], color='red', linewidth=1.5,label='LU-SGS 2nd')
     line, = ax.plot(x, results2[99][:, 0], color='blue', linewidth=1.5,label = 'LU-SGS 1st')
 
     # ani = animation.FuncAnimation(